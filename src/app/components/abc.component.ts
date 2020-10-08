690841f6d0c31d4ff2789dd3a75c6d67c366dce4
shagungarg <shagungarg@shagungargs-MacBook-Pro.local>
2020-10-08 14:23:32 +0530

    string interpolation

--- src/app/components/abc.component.ts
+++ src/app/components/abc.component.ts
@@ -2,8 +2,15 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-abc',
-  template: `<h2>Hello i am abc component</h2>`,
+  template: `<h3>{{'Welcome!'}} {{userName}} with id: {{userId}}
+    load with function: {{getUserName()}}</h3>`,
   styles: [``]
 })
 export class AbcComponent {
+  userName = 'shagun';
+  userId = 10;
+
+  getUserName() {
+    return this.userName;
+  }
 }
