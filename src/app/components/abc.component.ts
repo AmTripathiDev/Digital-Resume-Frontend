7fb50b165cc1efe967260b09ab3f2476e3d71452
shagungarg <shagungarg@shagungargs-MacBook-Pro.local>
2020-10-08 14:31:49 +0530

    property binding

--- src/app/components/abc.component.ts
+++ src/app/components/abc.component.ts
@@ -3,12 +3,15 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-abc',
   template: `<h3>{{'Welcome!'}} {{userName}} with id: {{userId}}
-    load with function: {{getUserName()}}</h3>`,
+    load with function: {{getUserName()}}</h3>
+  <button [disabled]="!isButtonDisabled">Disable me</button>
+  `,
   styles: [``]
 })
 export class AbcComponent {
   userName = 'shagun';
   userId = 10;
+  isButtonDisabled = true;
 
   getUserName() {
     return this.userName;
