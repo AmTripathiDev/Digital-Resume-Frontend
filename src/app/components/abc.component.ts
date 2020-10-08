cd3f067a5148fd28fc57250d4bfbd7bb021adda9
shagungarg <shagungarg@shagungargs-MacBook-Pro.local>
2020-10-08 14:54:01 +0530

    event Binding

--- src/app/components/abc.component.ts
+++ src/app/components/abc.component.ts
@@ -4,16 +4,21 @@ import {Component} from '@angular/core';
   selector: 'app-abc',
   template: `<h3>{{'Welcome!'}} {{userName}} with id: {{userId}}
     load with function: {{getUserName()}}</h3>
-  <button [disabled]="!isButtonDisabled">Disable me</button>
+  <button (click)="toggleButton()" [disabled]="isButtonDisabled">{{isButtonDisabled ? "Enable me" : "Disable Me" }}</button>
+  <button (click)="this.isButtonDisabled = false">Enable Button</button>
   `,
   styles: [``]
 })
 export class AbcComponent {
   userName = 'shagun';
   userId = 10;
-  isButtonDisabled = true;
+  isButtonDisabled = false;
 
   getUserName() {
     return this.userName;
+  }
+
+  toggleButton() {
+    this.isButtonDisabled = !this.isButtonDisabled;
   }
 }
