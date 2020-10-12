90752a1b3bcaaaaf6c4bbf2d17d82e493e8e552e
shagungarg <shagungarg@shagungargs-MacBook-Pro.local>
2020-10-12 17:52:34 +0530

    creating login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,30 +3,38 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Material</h1>
-    <mat-toolbar style="background:#a7a7a7">
-      <button mat-button color="primary">Home</button>
-      <button mat-button color="primary">Contact Us</button>
-      <button mat-button color="accent">Login</button>
-    </mat-toolbar>
-    <mat-card fxLayoutAlign="center" fxLayout="column" style="width: 30vw;margin-left: 2%;margin-top: 2%;background: #9c27b0">
-      <div fxLayout="row" fxLayoutGap="20px">
+    <div class="overlay" fxLayoutAlign="center center" fxLayout="column" fxLayoutGap="40px">
+      <img width="20%" src="../assets/digiresume-green.png">
+      <mat-card fxLayout="column">
+        <h2>Login</h2>
         <mat-form-field>
-          <mat-label>First name</mat-label>
-          <input matInput placeholder="FirstName">
+          <input type="email" matInput placeholder="Email"/>
         </mat-form-field>
         <mat-form-field>
-          <mat-label>Last Name</mat-label>
-          <input matInput placeholder="Last Name">
+          <input type="password" matInput placeholder="Password"/>
         </mat-form-field>
-      </div>
-      <mat-form-field>
-        <mat-label>Email</mat-label>
-        <input matInput placeholder="Email">
-      </mat-form-field>
-    </mat-card>
+        <a href="#">Forgot Password?</a>
+        <div style="margin-top: 2rem" fxLayout="row" fxLayoutGap="20px" fxLayoutAlign="end">
+          <button color="primary" mat-raised-button>Login</button>
+          <button color="accent" mat-raised-button>Signup</button>
+        </div>
+      </mat-card>
+    </div>
   `,
   styles: [`
+    .overlay {
+      width: 100%;
+      height: 100%;
+    }
+
+    button {
+      color: white !important;
+    }
+
+    mat-card {
+      height: 20rem;
+      width: 25rem;
+    }
   `]
 })
 export class AppComponent {
