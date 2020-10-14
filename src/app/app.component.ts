40368c4dcfaef946392e440fdf9278f84cd45d9e
shagungarg <shagungarg@shagungargs-MacBook-Pro.local>
2020-10-14 16:04:09 +0530

    switch map operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {filter} from 'rxjs/operators';
+import {filter, switchMap} from 'rxjs/operators';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -51,25 +52,39 @@ export class AppComponent {
       email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, [Validators.required, Validators.maxLength(12), Validators.minLength(8)])
     });
+    this.buySugarFromShop();
+  }
+
+  buySugarInBulk() {
+    return new Observable(emitter => {
+      emitter.next('Sugar is purchased');
+    });
+  }
+
+  buySugarInQuantity(quantity) {
+    return new Observable(emitter => {
+      emitter.next('Sugar with Quantity: ' + quantity + ' is here for you');
+    });
+  }
+
+  buySugarFromShop() {
+    // observable A is dependant on Observable B
+    // we need to observe value of B only
+    // this.buySugarInBulk().subscribe(data => {
+    //   this.buySugarInQuantity('1Kg').subscribe(res => {
+    //     console.log(res);
+    //   });
+    // });
+    // const newObserver = this.buySugarInBulk().pipe(switchMap(() => {
+    //   return this.buySugarInQuantity('1kg');
+    // }));
+    // newObserver.subscribe((data) => {
+    //   console.log(data);
+    // });
   }
 
   login() {
-    // using map
-    // observable is throwing -> email & password
-    // const mapObserver = this.loginForm.valueChanges.pipe(map(data => {
-    //   return data.email;
-    // }));
-    // mapObserver.subscribe(data => {
-    //   console.log(data);
-    // });
 
-    // using filter
-    // const filterObserver = this.loginForm.valueChanges.pipe(filter(data => {
-    //   return data.email === 'shagungarg2010@gmail.com';
-    // }));
-    // filterObserver.subscribe(data => {
-    //   console.log(data);
-    // });
   }
 
   signup() {
