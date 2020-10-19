c0f8d95af16c633db5a9229ca304b81e668d34fc
shagungarg <shagungarg@shagungargs-MacBook-Pro.local>
2020-10-20 00:13:22 +0530

    forgot password part 2

--- src/app/container/forgot-password.component.ts
+++ src/app/container/forgot-password.component.ts
@@ -6,16 +6,35 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
   template: `
     <form fxLayoutAlign="center center"
           fxLayout="column"
-          fxLayoutGap="40px" class="overlay" [formGroup]="forgotPasswordForm" (ngSubmit)="forgotPasswordForm && sendEmail()">
+          fxLayoutGap="40px" class="overlay" [formGroup]="forgotPasswordForm"
+          (ngSubmit)="forgotPasswordForm && !this.isEmailSent ? sendEmail(): changePassword()">
       <img width="20%" src="../assets/digiresume-green.png">
       <mat-card fxLayout="column">
         <h2>Forgot Password</h2>
-        <mat-form-field>
+        <mat-form-field *ngIf="!this.isEmailSent">
           <input formControlName="email" type="email" matInput placeholder="Email"/>
           <mat-error>Valid Email is Required</mat-error>
         </mat-form-field>
-        <div style="margin-top: 2rem" fxLayout="row" fxLayoutGap="20px" fxLayoutAlign="end">
+        <div fxLayout="column" *ngIf="isEmailSent">
+          <mat-form-field>
+            <input formControlName="code" matInput placeholder="Enter Code"/>
+            <mat-error>Valid Code is Required</mat-error>
+          </mat-form-field>
+          <mat-form-field>
+            <input formControlName="new_password" matInput placeholder="New Password"/>
+            <mat-error>Valid Password is Required</mat-error>
+          </mat-form-field>
+          <mat-form-field>
+            <input formControlName="confirm_password" matInput placeholder="Confirm Password"/>
+            <mat-error>Valid Password is Required</mat-error>
+          </mat-form-field>
+        </div>
+        <div *ngIf="!this.isEmailSent" style="margin-top: 2rem" fxLayout="row" fxLayoutGap="20px" fxLayoutAlign="end">
           <button type="submit" color="primary" mat-raised-button>Send Email</button>
+        </div>
+        <div *ngIf="this.isEmailSent" style="margin-top: 2rem" fxLayout="row" fxLayoutGap="20px" fxLayoutAlign="end">
+          <button type="submit" color="primary" mat-raised-button>Change Password</button>
+          <button color="accent" mat-raised-button>Go to Login</button>
         </div>
         <mat-spinner *ngIf="this.loading" color="accent" diameter="40"></mat-spinner>
       </mat-card>
@@ -38,7 +57,7 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
     }
 
     mat-card {
-      height: 15rem;
+      height: auto;
       width: 25rem;
     }
   `]
@@ -47,15 +66,26 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 export class ForgotPasswordComponent {
   loading = false;
   forgotPasswordForm: FormGroup;
+  isEmailSent = false;
 
   constructor() {
     this.forgotPasswordForm = new FormGroup({
-      email: new FormControl(null, [Validators.required]),
+      email: new FormControl(null, !this.isEmailSent ? [Validators.required] : []),
+      code: new FormControl(null, []),
+      new_password: new FormControl(null, []),
+      confirm_password: new FormControl(null, []),
     });
   }
 
+
   sendEmail() {
-    console.log('send email');
+    this.isEmailSent = true;
+    this.forgotPasswordForm.get('code').setValidators([Validators.required]);
+    this.forgotPasswordForm.get('new_password').setValidators([Validators.required]);
+    this.forgotPasswordForm.get('confirm_password').setValidators([Validators.required]);
+  }
+
+  changePassword() {
   }
 }
 
