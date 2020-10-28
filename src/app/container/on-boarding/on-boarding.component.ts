c920fdf75c0661a146540202c37a426a0d0efcc7
shagungarg <shagungarg@shagungargs-MacBook-Pro.local>
2020-10-28 06:54:24 +0530

    ONBOARDING:Logic for completed step

--- src/app/container/on-boarding/on-boarding.component.ts
+++ src/app/container/on-boarding/on-boarding.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {Resume} from '../../models/resume';
 
 @Component({
   selector: 'app-on-boarding',
@@ -8,13 +9,16 @@ import {Component} from '@angular/core';
         <ng-template matStepLabel>Name your Resume</ng-template>
         <app-resume-name></app-resume-name>
       </mat-step>
+      <mat-step>
+        <ng-template matStepLabel>Upload Video & Image</ng-template>
+        <p>Upload Video and image</p>
+      </mat-step>
     </mat-horizontal-stepper>
   `,
   styles: [``]
 })
 
 export class OnBoardingComponent {
-
   constructor() {
   }
 }
--- src/app/container/on-boarding/resume-name.component.ts
+++ src/app/container/on-boarding/resume-name.component.ts
@@ -1,12 +1,13 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Resume} from '../../models/resume';
 
 @Component({
   selector: 'app-resume-name',
   template: `
     <div class="container" fxLayoutAlign="center center" fxLayout="column">
       <form (ngSubmit)="this.resumeForm.valid && this.createResume()" [formGroup]="this.resumeForm">
-        <mat-card fxFlex="100%" fxLayout="column" fxLayoutGap="20px">
+        <mat-card *ngIf="!isCompleted" fxFlex="100%" fxLayout="column" fxLayoutGap="20px">
           <h1 style="font-size: 2rem">Name your Resume</h1>
           <mat-form-field>
             <input formControlName="name"
@@ -15,9 +16,14 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
             <mat-error>This field is Required</mat-error>
           </mat-form-field>
           <div fxLayoutAlign="end end">
-            <button matStepperNext type="submit" mat-raised-button color="accent">NEXT</button>
+            <button matStepperNext type="submit" mat-raised-button color="accent">Save</button>
           </div>
         </mat-card>
+        <div *ngIf="isCompleted" fxLayout="column" fxLayoutGap="30px" fxLayoutAlign="start center">
+          <img width="8%" src="../../../assets/complete.png"/>
+          <p>You Have already Completed This Step</p>
+          <button color="accent" mat-raised-button matStepperNext type="button">Next</button>
+        </div>
       </form>
     </div>
   `,
@@ -31,6 +37,8 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 export class ResumeNameComponent implements OnInit {
   resumeForm: FormGroup;
+  isCompleted = false;
+  loading = false;
 
   constructor() {
   }
@@ -42,6 +50,7 @@ export class ResumeNameComponent implements OnInit {
   }
 
   createResume() {
-    console.log('called');
+    // actual m data ko save kro using an api
+    this.isCompleted = true;
   }
 }
