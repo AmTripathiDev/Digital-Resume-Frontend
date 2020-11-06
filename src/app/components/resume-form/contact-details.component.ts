1235ac656e83aa6f7c7a88eb6be2172a51d244fc
shagungarg <shagungarg@shagungargs-MacBook-Pro.local>
2020-11-06 15:43:42 +0530

    creating education form card view

--- src/app/components/resume-form/contact-details.component.ts
+++ src/app/components/resume-form/contact-details.component.ts
@@ -12,18 +12,18 @@ import {ContactDetailFormComponent} from './resume-dialogues/contact-detail-form
           <h3 class="empty-heading">No Contact Details Added Yet</h3>
         </ng-container>
         <ng-container *ngIf="this.contactDetails">
-          <h3 style="margin: 0;font-weight: bold">{{this.contactDetails.first_name}}</h3>
-          <h3 style="margin: 0;font-weight: bold">{{this.contactDetails.last_name}}</h3>
-          <p style="margin: 0">{{this.contactDetails.address}}</p>
-          <p style="margin: 0">{{this.contactDetails.city}}</p>
-          <p style="margin: 0">{{this.contactDetails.country}}</p>
+          <h3 class="resume-form-info-card-heading">{{this.contactDetails.first_name}}</h3>
+          <h3 class="resume-form-info-card-heading">{{this.contactDetails.last_name}}</h3>
+          <p class="resume-form-info-card-heading">{{this.contactDetails.address}}</p>
+          <p class="resume-form-info-card-heading">{{this.contactDetails.city}}</p>
+          <p class="resume-form-info-card-heading">{{this.contactDetails.country}}</p>
         </ng-container>
         <div fxLayoutAlign="center center">
           <div class="hover-container">
             <div class="hover">
-              <button (click)="openContactForm()" style=" margin-top: 2.5rem" mat-icon-button>
-                <mat-icon *ngIf="!this.contactDetails">add</mat-icon>
-                <mat-icon *ngIf="this.contactDetails">create</mat-icon>
+              <button class="resume-form-mat-icon-button" (click)="openContactForm()" mat-icon-button>
+                <mat-icon class="resume-form-mat-icon" *ngIf="!this.contactDetails">add</mat-icon>
+                <mat-icon class="resume-form-mat-icon" *ngIf="this.contactDetails">create</mat-icon>
               </button>
             </div>
           </div>
@@ -38,10 +38,6 @@ import {ContactDetailFormComponent} from './resume-dialogues/contact-detail-form
       margin-top: 2%;
       text-align: center;
       font-weight: bold
-    }
-
-    mat-icon {
-      font-size: 43px;
     }
   `]
 })
